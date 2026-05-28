9ecc05e31b7e3b7375edd5a92fc034b9fc60ecc9
sakshidhawale5004 <sakshi.dhawale5004@gmail.com>
2026-05-28 11:32:17 +0530

    Update navbar and footer to use new Dermatiqua logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Menu, X, Phone } from "lucide-react";
-import logo from "@/assets/logofinal.jpeg";
+import logo from "@/assets/dermatiqua-logo.png";
 
 const links = [
   { to: "/", label: "Home" },
